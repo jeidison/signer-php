856c0a835715d9575d2180227b9f0a33530eb3f5
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-05-26 19:07:39 -0300

    test(xref14): cover prev pointer to xref stream object
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- tests/Unit/XRef14ParserTest.php
+++ tests/Unit/XRef14ParserTest.php
@@ -23,6 +23,26 @@ final class XRef14ParserTest extends TestCase
         self::assertSame('1.4', $result->minimumPdfVersion);
     }
 
+    public function test_parse_merges_previous_xref_stream_when_prev_points_to_xref_object(): void
+    {
+        $previousEntries = chr(1).pack('n', 10).chr(0);
+        $previous = "14 0 obj\n"
+            ."<< /Type /XRef /W [1 2 1] /Size 2 /Index [1 1] /Length 4 >>\n"
+            ."stream\n"
+            .$previousEntries."\n"
+            ."endstream\n"
+            ."endobj\n";
+
+        $current = "xref\n2 1\n0000000020 00000 n \ntrailer\n<< /Size 3 /Prev 0 >>\nstartxref\n123\n%%EOF\n";
+        $buffer = $previous.$current;
+        $currentPos = strlen($previous);
+
+        $result = (new XRef14Parser)->parse($buffer, $currentPos);
+
+        self::assertSame(10, $result->table[1]);
+        self::assertSame(20, $result->table[2]);
+    }
+
     public function test_parse_throws_when_xref_tag_is_missing_at_position(): void
     {
         $buffer = "no-tag-here\ntrailer\n<< /Size 1 >>\nstartxref\n0\n%%EOF\n";
